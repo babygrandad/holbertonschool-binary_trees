2480b9acb925dc634dbbc9e531a7ae18c7f6dda3
babygrandad <relebogilenkotswe@gmail.com>
2024-04-22 08:40:49 +0000

    Uncle / Malome fixed as well as 17 sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,24 +10,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
+	if (node == node->parent->left)
 		return (node->parent->right);
 	else
 		return (node->parent->left);
-}
-
-/**
- * binary_tree_uncle - get uncle of a node.
- *
- * @node: pointer node to get uncle.
- *
- * Return: pointer to uncle node, NULL otherwise.
- */
-
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-{
-	if (node == NULL || node->parent == NULL)
-		return (NULL);
-
-	return (binary_tree_sibling(node->parent));
 }
